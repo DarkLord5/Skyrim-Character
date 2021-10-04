3a694dbfe38abf6da2b95c6f90170692036d494b
ArtemNoreyko <ArtemNoreyko@DESKTOP-IQF4D8H>
2021-10-04 19:41:33 +0300

    adding files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SkyrimPlayer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a language: 1)English(eng). 2)Russian(ru)");
+            string language = Console.ReadLine();
+            MyCharacter Pidor = new MyCharacter(language);
+            Pidor.CreateCaracter();
+
+            Console.ReadKey();
+        }
+    }
+}
